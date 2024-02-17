30ba3f84e7b8601d8993517a5276658678ff1d44
Lora <143578889+Lora207@users.noreply.github.com>
2024-02-17 15:53:23 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,41 @@
+
+    }
+    public static void main(String[] args) {
+}
+
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+        }function main() {
+    }
+}
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+
+    });
+    for (let i = 0; i < 10; i++) {
+    return data;
+    const data = generateRandomData();
+
+function generateRandomData() {
+
+    const data = [];    }
+
+        }
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+
+}
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+        for (int item : data) {
+        }
+    public static void main(String[] args) {
